05036b8b1b878ef43359fc6b354b7b3077f23526
Eric Moore <ericmoore0709@gmail.com>
2024-07-08 09:50:05 -0700

    JSON fixes

--- routes/companies.js
+++ routes/companies.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res, next) => {
         // get list of companies
         const companies = await db.query('SELECT * FROM companies;');
         // return list of companies
-        return res.status(200).json(companies.rows);
+        return res.status(200).json({ companies: companies.rows });
     } catch (err) {
         console.log(err);
         return next(err);
@@ -22,7 +22,7 @@ router.get('/:code', async (req, res, next) => {
 
     try {
         const company = await db.query('SELECT * FROM companies WHERE code = $1 LIMIT 1;', [code]);
-        return res.status(200).json(company.rows);
+        return res.status(200).json({ company: company.rows[0] });
     } catch (err) {
         console.log(err);
         return next(err);
