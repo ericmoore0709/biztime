e2b3c35bdc8c117948128a13a31906c29bb9c2dc
Eric Moore <ericmoore0709@gmail.com>
2024-07-12 12:50:24 -0700

    Finished invoice routes

--- routes/invoices.js
+++ routes/invoices.js
@@ -63,7 +63,54 @@ router.put('/:id', async (req, res, next) => {
 
     const id = req.params.id;
 
-    
+    try {
+
+        const exists = await db.query('SELECT * FROM invoices WHERE id = $1;', [id]);
+
+        if (!exists.rowCount)
+            return res.status(404).json({ error: "Invoice not found at given ID." });
+
+        const { amt } = req.body;
+
+        if (!amt)
+            return res.status(400).json({ error: "amt is required." });
+
+        const result = await db.query('UPDATE invoices SET amt = $1 WHERE id = $2 RETURNING *;', [amt, id]);
+
+        if (result.rowCount)
+            return res.status(200).json({ invoice: result.rows[0] });
+
+        return res.status(400)
+
+    } catch (err) {
+        console.log(err);
+        return next(err);
+    }
+
+});
+
+router.delete('/:id', async (req, res, next) => {
+
+    const id = req.params.id;
+
+    try {
+
+        const exists = await db.query('SELECT * FROM invoices WHERE id = $1;', [id]);
+
+        if (!exists.rowCount)
+            return res.status(404).json({ error: "Not found." });
+
+        const result = await db.query('DELETE FROM invoices WHERE id = $1', [id]);
+
+        if (result)
+            return res.status(200).json({ status: 'deleted' });
+
+        return res.status(500).json({ status: 'failed to delete' });
+
+    } catch (err) {
+        console.log(err);
+        return next(err);
+    }
 
 });
 
